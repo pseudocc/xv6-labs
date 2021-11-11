62b60b2d8c0fdec1db2a9ad23c1b8ec7db815a8b
pseudocc <pseudoc@163.com>
2021-11-11 04:29:40 +0000

    lab utils: 1 sleep

---
+++ user/sleep.c
@@ -0,0 +1,17 @@
+#include "kernel/types.h"
+#include "kernel/stat.h"
+#include "user/user.h"
+
+int main(int argc, char *argv[]) {
+  int i;
+  char say[] = "(nothing happens for a little while)\n";
+  if (argc < 2)
+    exit(1);
+  
+  i = atoi(argv[1]);
+  write(1, say, sizeof(say));
+  sleep(i);
+
+  exit(0);
+}
+ 
